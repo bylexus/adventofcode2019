401848d833a02efe533c8efb6b46ef36f9032e07
Alexander Schenkel <aschenkel@kadenpartner.ch>
2019-12-07 17:32:26 +0100

    Refactoring of day 7 done

--- src/day_07.cpp
+++ src/day_07.cpp
@@ -1,28 +1,59 @@
 #include <iostream>
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <math.h>
 #include <vector>
-#include <map>
+#include <queue>
 #include "common.h"
 
 using namespace std;
 
 /**
- * Day 7 - Amplification Circuit
+ * Day 7 Part 2 - Amplification Circuit
+ *
+ * I introduced a Program class here, to keep all the program related
+ * information (data, instruction pointer, input values) in one place for
+ * each AMP (= Program). This makes things a lot easier.
  */
 
 typedef vector<long> memory_t;
 
-// Globals, used on all programs
-vector<long> inputData{0, 0};
-vector<long>::size_type inputDataPointer = 0;
-long output;
+/**
+ * An AMP can be in one of the following states:
+ * - P_RUN: still running
+ * - P_OUTPUT: Program has generated an output, and is halted until continued
+ * - P_HALT: The program is done, no furter run
+ */
+enum ProgramResult
+{
+    P_RUN,
+    P_OUTPUT,
+    P_HALT
+};
+
+class Program
+{
+public:
+    int pNr = 0;
+    memory_t *memory;
+    memory_t::size_type iPointer;
+    queue<long> inputValues;
+    long output;
+    ProgramResult state;
+
+    Program(memory_t *mem)
+    {
+        memory = new memory_t(*mem);
+        iPointer = 0;
+        state = P_RUN;
+    }
+
+    ~Program()
+    {
+        memory->clear();
+        delete memory;
+    }
+};
 
 /**
- * extracts thae param modes from the whole opcode number.
- * j
+ * extracts the param modes from the whole opcode number.
  * Opcode: The last 2 digits are the opcode, the rest is param modes, e.g.:
  * "10102": --> "101" are param modes, "02" is the opcode 2.
  * The param modes are read right-to-left: the most right mode digit is the mode for the 1st param,
@@ -85,21 +116,29 @@ long getValue(memory_t &data, memory_t::size_type iPointer, int paramMode)
 }
 
 /**
- * Executes a single instruction:
- * @param iPointer The actual instruction pointer
- * @param data The memory. iPointer should now point to an opcode value
+ * Executes a single instruction (the next according to the Program's iPointer),
+ * sets the output value (if applicable) and sets the iPointer to the next program
+ * address.
  */
-memory_t::size_type executeInstruction(memory_t::size_type iPointer, memory_t &data)
+void executeNextInstruction(Program *program)
 {
     // Opcode: The last 2 digits are the opcode, the rest is param modes, e.g.:
     // "10102": --> "101" are param modes, "02" is the opcode 2.
     // The param modes are read right-to-left: the most right mode digit is the mode for the 1st param,
     // the next to the right is the param mode for the 2nd param and so on (so it's the reverse direction of the params, neat :-))
-    long opcode = data[iPointer] % 100; // last 2 digits are the opcode
+    memory_t &data = *(program->memory);
+    long opcode = data[program->iPointer] % 100; // last 2 digits are the opcode
     // cout << "I:" << iPointer << ": OP:" << opcode << "(" << data[iPointer] << ")" << endl;
 
+    // cout << "  opcode: " << opcode;
+    if (program->iPointer >= data.size())
+    {
+        program->state = P_HALT;
+        return;
+    }
+
     vector<int> paramModes{0, 0, 0};
-    readParamModesFromOpcode(data[iPointer], paramModes);
+    readParamModesFromOpcode(data[program->iPointer], paramModes);
 
     long val1;
     long val2;
@@ -109,66 +148,68 @@ memory_t::size_type executeInstruction(memory_t::size_type iPointer, memory_t &d
     {
     // Add instr:
     case 1:
-        val1 = getValue(data, iPointer + 1, paramModes[0]);
-        val2 = getValue(data, iPointer + 2, paramModes[1]);
-        storePos = data[iPointer + 3];
+        val1 = getValue(data, program->iPointer + 1, paramModes[0]);
+        val2 = getValue(data, program->iPointer + 2, paramModes[1]);
+        storePos = data[program->iPointer + 3];
         data[storePos] = val1 + val2;
-        iPointer += 4;
+        program->iPointer += 4;
         break;
     // Multiply instr:
     case 2:
-        val1 = getValue(data, iPointer + 1, paramModes[0]);
-        val2 = getValue(data, iPointer + 2, paramModes[1]);
-        storePos = data[iPointer + 3];
+        val1 = getValue(data, program->iPointer + 1, paramModes[0]);
+        val2 = getValue(data, program->iPointer + 2, paramModes[1]);
+        storePos = data[program->iPointer + 3];
         data[storePos] = val1 * val2;
-        iPointer += 4;
+        program->iPointer += 4;
         break;
     // Input instr.
     case 3:
-        inputValue = inputData[inputDataPointer++];
-        storePos = data[iPointer + 1];
+        inputValue = program->inputValues.front();
+        program->inputValues.pop();
+        storePos = data[program->iPointer + 1];
         data[storePos] = inputValue;
         // cout << "    Input value taken: " << inputValue << ", store at: " << storePos << endl;
-        iPointer += 2;
+        program->iPointer += 2;
         break;
     // Output instr.
     case 4:
-        val1 = getValue(data, iPointer + 1, paramModes[0]);
-        cout << "    Output: " << val1 << endl;
-        output = val1;
-        iPointer += 2;
+        val1 = getValue(data, program->iPointer + 1, paramModes[0]);
+        // cout << "    Output: " << val1 << endl;
+        program->output = val1;
+        program->iPointer += 2;
+        program->state = P_OUTPUT;
         break;
     // Jump if true:
     case 5:
-        val1 = getValue(data, iPointer + 1, paramModes[0]);
-        val2 = getValue(data, iPointer + 2, paramModes[1]);
+        val1 = getValue(data, program->iPointer + 1, paramModes[0]);
+        val2 = getValue(data, program->iPointer + 2, paramModes[1]);
         if (val1 != 0)
         {
-            iPointer = val2;
+            program->iPointer = val2;
         }
         else
         {
-            iPointer += 3;
+            program->iPointer += 3;
         }
         break;
     // Jump if false:
     case 6:
-        val1 = getValue(data, iPointer + 1, paramModes[0]);
-        val2 = getValue(data, iPointer + 2, paramModes[1]);
+        val1 = getValue(data, program->iPointer + 1, paramModes[0]);
+        val2 = getValue(data, program->iPointer + 2, paramModes[1]);
         if (val1 == 0)
         {
-            iPointer = val2;
+            program->iPointer = val2;
         }
         else
         {
-            iPointer += 3;
+            program->iPointer += 3;
         }
         break;
     // less than
     case 7:
-        val1 = getValue(data, iPointer + 1, paramModes[0]);
-        val2 = getValue(data, iPointer + 2, paramModes[1]);
-        storePos = data[iPointer + 3];
+        val1 = getValue(data, program->iPointer + 1, paramModes[0]);
+        val2 = getValue(data, program->iPointer + 2, paramModes[1]);
+        storePos = data[program->iPointer + 3];
         if (val1 < val2)
         {
             data[storePos] = 1;
@@ -177,13 +218,13 @@ memory_t::size_type executeInstruction(memory_t::size_type iPointer, memory_t &d
         {
             data[storePos] = 0;
         }
-        iPointer += 4;
+        program->iPointer += 4;
         break;
     // equals:
     case 8:
-        val1 = getValue(data, iPointer + 1, paramModes[0]);
-        val2 = getValue(data, iPointer + 2, paramModes[1]);
-        storePos = data[iPointer + 3];
+        val1 = getValue(data, program->iPointer + 1, paramModes[0]);
+        val2 = getValue(data, program->iPointer + 2, paramModes[1]);
+        storePos = data[program->iPointer + 3];
         if (val1 == val2)
         {
             data[storePos] = 1;
@@ -192,32 +233,38 @@ memory_t::size_type executeInstruction(memory_t::size_type iPointer, memory_t &d
         {
             data[storePos] = 0;
         }
-        iPointer += 4;
+        program->iPointer += 4;
         break;
     case 99:
-        iPointer = data.size();
+        program->iPointer = data.size();
+        program->state = P_HALT;
         break;
     default:
         cerr << "Error: unknown opcode occured: " << opcode << endl;
         exit(1);
     }
-
-    return iPointer;
+    // cout << endl;
 }
 
 /**
- * Run program until the end, instruction-by-instruction
+ * Run a program until the end, instruction-by-instruction, until it is set to
+ * output or halt.
  */
-long runProgram(memory_t &data)
+void runProgram(Program *program)
 {
-    memory_t::size_type instructionPointer = 0;
-    while (instructionPointer < data.size())
+    program->state = P_RUN;
+    while (program->state == P_RUN)
     {
-        instructionPointer = executeInstruction(instructionPointer, data);
+        // cout << "P:" << program->pNr << endl;
+        executeNextInstruction(program);
+        // cout << "E:" << program->pNr <<  ", state: " << program->state << endl;
     }
-    return output;
+    // cout << "Exit: " << program->pNr << endl;
 }
 
+/**
+ * Create all permutations of the given vector
+ */
 void permute(vector<long> a, int l, int r, vector<vector<long>> &permutations)
 {
     // Base case
@@ -228,7 +275,6 @@ void permute(vector<long> a, int l, int r, vector<vector<long>> &permutations)
         // Permutations made
         for (int i = l; i <= r; i++)
         {
-
             // Swapping done
             swap(a[l], a[i]);
 
@@ -239,6 +285,115 @@ void permute(vector<long> a, int l, int r, vector<vector<long>> &permutations)
             swap(a[l], a[i]);
         }
     }
+}
+
+void runSolution1(memory_t &data)
+{
+    vector<long> inputs{0, 1, 2, 3, 4};
+    vector<vector<long>> inputPermutations;
+    vector<Program *> ampPrograms;
+
+    // Permute inputs:
+    permute(inputs, 0, 4, inputPermutations);
+
+    long highest = 0;
+    // Test 1:
+    // inputPermutations = vector<vector<long>>{{4,3,2,1,0}};
+    // Test 2:
+    // inputPermutations = vector<vector<long>>{{0,1,2,3,4}};
+    // Test 3:
+    // inputPermutations = vector<vector<long>>{{1,0,4,3,2}};
+    for (auto actPermutation : inputPermutations)
+    {
+        // Build amps:
+        for (auto p : ampPrograms)
+        {
+            delete p;
+        }
+        ampPrograms.clear();
+
+        // Create 5 new amp programs:
+        for (int i = 0; i < 5; i++)
+        {
+            Program *p = new Program(&data);
+            p->inputValues.push(actPermutation[i]);
+            p->pNr = i + 1;
+            ampPrograms.push_back(p);
+        }
+        // Run programs on each amp:
+        long inputValue = 0;
+        for (vector<long>::size_type i = 0; i < ampPrograms.size(); i++)
+        {
+            ampPrograms[i]->inputValues.push(inputValue);
+            runProgram(ampPrograms[i]);
+            inputValue = ampPrograms[i]->output;
+        }
+        if (inputValue > highest)
+        {
+            highest = inputValue;
+        }
+    }
+    cout << "Solution 1: Highest signal: " << highest << endl;
+}
+
+void runSolution2(memory_t &data)
+{
+    vector<long> inputs{5, 6, 7, 8, 9};
+    vector<vector<long>> inputPermutations;
+    vector<Program *> ampPrograms;
+
+    // Permute inputs:
+    permute(inputs, 0, 4, inputPermutations);
+
+    long highest = 0;
+    // Test 1:
+    // inputPermutations = vector<vector<long>>{{9,8,7,6,5}};
+    // Test 2:
+    // inputPermutations = vector<vector<long>>{{9,7,8,5,6}};
+    for (auto actPermutation : inputPermutations)
+    {
+        // Build amps:
+        for (auto p : ampPrograms)
+        {
+            delete p;
+        }
+        ampPrograms.clear();
+
+        // Create 5 new amp programs:
+        for (int i = 0; i < 5; i++)
+        {
+            Program *p = new Program(&data);
+            p->inputValues.push(actPermutation[i]);
+            p->pNr = i + 1;
+            if (i == 0)
+            {
+                p->inputValues.push(0);
+            }
+            ampPrograms.push_back(p);
+        }
+
+        // Run programs on each amp with feedback loop:
+        unsigned long i = 0;
+        unsigned long nextAmpIndex = 0;
+        Program *actAmp;
+        Program *nextAmp;
+        while (ampPrograms.back()->state != P_HALT)
+        {
+            i = i % ampPrograms.size(); // act amp, feedback loop counter
+            nextAmpIndex = (i + 1) % ampPrograms.size();
+            actAmp = ampPrograms[i];
+            nextAmp = ampPrograms[nextAmpIndex];
+            runProgram(actAmp);
+            // Feed the output of the act amp to the input of the next amp:
+            nextAmp->inputValues.push(actAmp->output);
+            i++;
+        }
+        if (ampPrograms.back()->output > highest)
+        {
+            highest = ampPrograms.back()->output;
+        }
+    }
+    std::cout << "Solution 2: Highest signal: " << highest << endl;
 }
 
 int main(int argc, char *args[])
@@ -252,97 +407,7 @@ int main(int argc, char *args[])
     // Read input file:
     memory_t data;
     readData<long>(args[1], ',', data);
-    vector<long> inputs{0,1,2,3,4};
-    vector<vector<long>> inputPermutations;
-    vector<memory_t> ampPrograms;
 
-    // Permute inputs:
-    permute(inputs, 0, 4, inputPermutations);
-
-    // Build amps:
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     ampPrograms.push_back(memory_t(data));
-    // }
-
-    // Test 1:
-    // string actPermutation = "43210";
-    // for (vector<long>::size_type i = 0; i < ampPrograms.size(); i++)
-    // {
-    //     inputDataPointer = 0;
-    //     inputData[0] = stol(actPermutation.substr(i,1));
-    //     cout << "Input data: " << inputData[0] << endl;
-    //     inputData[1] = runProgram(ampPrograms[i]);
-    // }
-    // cout << "Output after run: " << inputData[1] << endl;
-
-    // Test 2:
-    // string actPermutation = "01234";
-    // for (vector<long>::size_type i = 0; i < ampPrograms.size(); i++)
-    // {
-    //     inputDataPointer = 0;
-    //     inputData[0] = stol(actPermutation.substr(i,1));
-    //     cout << "Input data: " << inputData[0] << endl;
-    //     inputData[1] = runProgram(ampPrograms[i]);
-    // }
-    // cout << "Output after run: " << inputData[1] << endl;
-
-    // Test 3:
-    // string actPermutation = "10432";
-    // for (vector<long>::size_type i = 0; i < ampPrograms.size(); i++)
-    // {
-    //     inputDataPointer = 0;
-    //     inputData[0] = stol(actPermutation.substr(i,1));
-    //     cout << "Input data: " << inputData[0] << endl;
-    //     inputData[1] = runProgram(ampPrograms[i]);
-    // }
-    // cout << "Output after run: " << inputData[1] << endl;
-
-
-    long highest = 0;
-    // Test 1:
-    // inputPermutations = vector<vector<long>>{{4,3,2,1,0}};
-    // Test 2:
-    // inputPermutations = vector<vector<long>>{{0,1,2,3,4}};
-    // Test 3:
-    // inputPermutations = vector<vector<long>>{{1,0,4,3,2}};
-    for (auto actPermutation : inputPermutations)
-    {
-        // Build amps:
-        ampPrograms.clear();
-        inputDataPointer = 0;
-        inputData[0] = 0;
-        inputData[1] = 0;
-        for (int i = 0; i < 5; i++)
-        {
-            ampPrograms.push_back(memory_t(data));
-        }
-        // Run programs on each amp:
-        for (vector<long>::size_type i = 0; i < ampPrograms.size(); i++)
-        {
-            inputDataPointer = 0;
-            inputData[0] = actPermutation[i];
-            cout << "Input data: " << inputData[0] << endl;
-            inputData[1] = runProgram(ampPrograms[i]);
-            cout << "Output data: " << inputData[1] << endl;
-        }
-        if (inputData[1] > highest) {
-            highest = inputData[1];
-        }
-    }
-    cout << "Solution 1: Highest signal: " << highest << endl;
-
-    // Solution 1: Input data: 1
-    // inputData = vector<long>{1};
-    // inputDataPointer = 0;
-    // memory_t workingData(data);
-    // runProgram(workingData);
-    // cout << "Solution 1: Data at pos 0 after program end: " << workingData[0] << endl << endl;
-
-    // Solution 2: Input data: 5
-    // inputData = vector<long>{5};
-    // inputDataPointer = 0;
-    // workingData = memory_t(data);
-    // runProgram(workingData);
-    // cout << "Solution 2: Data at pos 0 after program end: " << workingData[0] << endl;
+    runSolution1(data);
+    runSolution2(data);
 }
