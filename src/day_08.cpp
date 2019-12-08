8f793c7d73a03fb7cc16c5d280867d5770a4d878
Alexander Schenkel <aschenkel@kadenpartner.ch>
2019-12-08 10:00:17 +0100

    Day 8: Created a PNG output in addition to the ascii art, using CImg

--- src/day_08.cpp
+++ src/day_08.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <vector>
 #include "common.h"
+#include "CImg.h"
 
 using namespace std;
+using namespace cimg_library;
 
 /**
  * Day 8 - Space Image Format
+ *
+ * Dependency: CImg library, http://cimg.eu/
  */
 
-typedef vector<long> memory_t;
+typedef vector<long>
+    memory_t;
 
 int main(int argc, char *args[])
 {
@@ -26,7 +31,8 @@ int main(int argc, char *args[])
     unsigned int height = 6;
     unsigned int width = 25;
     // unsigned int layers = img.length() / (height * width);
-    char imagechar[height][width];
+    unsigned char imagechar[height][width];
+    CImg<unsigned char> pngImg(width+4, height+4, 1, 3, 0); // add a small border of 2 px per side
 
     cout << "Layers in img: " << img.length() / (6 * 25) << endl;
 
@@ -85,14 +91,19 @@ int main(int argc, char *args[])
     }
     cout << "Solution 1: N° of 1px * N° of 2px: " << solution << endl;
 
-
-    cout << "Solution 2:" << endl << endl;
+    cout << "Solution 2:" << endl
+         << endl;
     for (unsigned int y = 0; y < height; ++y)
     {
         for (unsigned int x = 0; x < width; ++x)
         {
+            pngImg(x+2, y+2, 0, 0) = (imagechar[y][x] == '1' ? 255 : 0);
+            pngImg(x+2, y+2, 0, 1) = (imagechar[y][x] == '1' ? 255 : 0);
+            pngImg(x+2, y+2, 0, 2) = (imagechar[y][x] == '1' ? 255 : 0);
             cout << (imagechar[y][x] == '1' ? '*' : ' ');
         }
         cout << endl;
     }
+    cout << "Your message is stored in the image message.png" << endl;
+    pngImg.save_png("message.png");
 }
